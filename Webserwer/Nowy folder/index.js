228e15c4ffb2cfafb52aec89019158e17b7721c8
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-21 21:47:47 +0200

    Update index.js

--- Webserwer/Nowy folder/index.js
+++ Webserwer/Nowy folder/index.js
@@ -4,9 +4,10 @@ const requestListener = (req, res) => {
   console.log('Ktoś wszedł na serwer.');
   res.writeHead(200);
 
-  if (req.url === 'kontakt')
-  res.write('<title>Node</title>')
-  res.end('<h1>Hello Node!</h1>');
+  if (req.url === 'kontakt') {
+    res.write('<title>Kontakt</title>')
+    res.end();
+  }
 }
 
 const server = http.createServer(requestListener);
