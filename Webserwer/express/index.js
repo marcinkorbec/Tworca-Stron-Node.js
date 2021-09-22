ce6476bc2cf30566de827d4b2af158c74707a27f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-22 17:29:30 +0200

    Update index.js

--- Webserwer/express/index.js
+++ Webserwer/express/index.js
@@ -1,1 +1,14 @@
+const express = require('express');
 const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+})
+
+app.get('/firmy/:name', (req, res) => {
+  const {name} = req.params.name;
+  res.send('Kontakt');
+})
+
+app.listen(port);
