94bd864b662b6404ff83f0d2513eb4e572139758
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-22 16:55:46 +0200

    add

--- Webserwer/Nowy folder/index.js
+++ Webserwer/Nowy folder/index.js
@@ -1,16 +1,24 @@
 const http = require('http');
+const port = 3000;
 
 const requestListener = (req, res) => {
   console.log('Ktoś wszedł na serwer.');
   res.writeHead(200);
 
-  if (req.url === 'kontakt') {
-    res.write('<title>Kontakt</title>')
+  if (req.url === '/kontakt') {
+    res.write('<h1>Kontakt</h1>')
+    res.end();
+  }
+  if (req.url === '/') {
+    res.write('<h1>Strona główa.</h1>')
+    res.end();
+  } else {
+    res.write('<h1>404.</h1>')
     res.end();
   }
 }
 
 const server = http.createServer(requestListener);
-server.listen(3000, () => {
-  console.log(`Serwer słucha na http://localhost:3000`);
+server.listen(port, () => {
+  console.log(`Serwer słucha na http://localhost:${port}`);
 });
