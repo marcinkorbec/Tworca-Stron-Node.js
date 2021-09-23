9ee479518ad416ba8f7bc932593f1341a9dbf3fd
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-23 16:09:32 +0200

    Update index.js

--- Webserwer/debugowanie/index.js
+++ Webserwer/debugowanie/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const chalk = require('chalk');
+
 const port = 3000;
 const app = express();
 
@@ -13,13 +16,15 @@ app.get('/firmy/:name', (req, res) => {
     { slug: 'asvorltd', name: 'Asvor LTD'},
     { slug: 'brukbet', name: 'Bruk-BET'},
   ];
+  console.log(chalk.blue(req.params));
 
   const company = companies.find(x => x.slug === name);
 
   if (!company) {
     res.send('Nie ma takiej firmy w bazie!')
+  } else {
+    res.send(`Nazwa firmy ${company.name}`);
   }
-  res.send(`Nazwa firmy ${company.name}`);
 })
 
 app.listen(port);
