daf70e15b0e4284971952a2dbac69fcdc182dba5
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-26 12:46:25 +0200

    add

---
+++ Webserwer/layouty/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const chalk = require('chalk');
+const port = 3000;
+const path = require('path');
+const app = express();
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname +'/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+
+app.get('/firmy/:name', (req, res) => {
+  debugger;
+  const { name } = req.params;
+  const companies = [
+    { slug: 'tworcastron', name: 'Twórca Stron.pl'},
+    { slug: 'asvorltd', name: 'Asvor LTD'},
+    { slug: 'brukbet', name: 'Bruk-BET'},
+  ];
+  console.log(chalk.bgRed(req.params.name));
+  const company = companies.find(x => x.slug === name);
+
+  if (!company) {
+    res.send('Nie ma takiej firmy w bazie!');
+    res.render(path.join(__dirname + 'views/company.html'))
+  } else {
+    res.send(`Nazwa firmy ${company.name}`);
+  }
+})
+
+app.listen(port);
+
+console.log('Polacy to debile');
