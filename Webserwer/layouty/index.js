47aabbf7a926733c4a59d2f86df4bf5b4d462743
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-26 18:09:54 +0200

    add

--- Webserwer/layouty/index.js
+++ Webserwer/layouty/index.js
@@ -8,26 +8,36 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname +'/views'));
 
+const companies = [
+  { slug: 'tworcastron', name: 'Twórca Stron.pl'},
+  { slug: 'asvorltd', name: 'Asvor LTD'},
+  { slug: 'brukbet', name: 'Bruk-BET'},
+];
+
 app.get('/', (req, res) => {
   res.render('home');
 })
 
+app.get('/firmy', (req, res) => {
+  let html = 'Lista firm: <br>';
+  companies.forEach(myFunction);
+  function myFunction() {
+    html += `<li>${companies.name}</li><br>`
+  };
+
+})
 
 app.get('/firmy/:name', (req, res) => {
   debugger;
   const { name } = req.params;
-  const companies = [
-    { slug: 'tworcastron', name: 'Twórca Stron.pl'},
-    { slug: 'asvorltd', name: 'Asvor LTD'},
-    { slug: 'brukbet', name: 'Bruk-BET'},
-  ];
+
   console.log(chalk.bgRed(req.params.name));
   const company = companies.find(x => x.slug === name);
   res.render('company', {name: company?.name});
 
 })
 app.get('*', (req, res) => {
-  res.render('');
+  res.render('errors/404');
 })
 app.listen(port);
 
