6fd3160a8e093fa8de7a9a38d8052ad63d88d5df
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-01 19:12:47 +0200

    po odcinku layout

--- Webserwer/layouty/index.js
+++ Webserwer/layouty/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const chalk = require('chalk');
 const port = 3000;
 const path = require('path');
+const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
 //view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname +'/views'));
+
+//set layout
+app.use(ejsLayouts);
+app.set('layout', './layouts/main.ejs')
 
 const companies = [
   { slug: 'tworcastron', name: 'Twórca Stron.pl'},
@@ -15,42 +20,39 @@ const companies = [
 ];
 
 app.get('/', (req, res) => {
-  res.render('home', {
+  res.render('pages/home', {
     title: 'Strona główna'
   });
-})
+});
 
 app.get(`/firmy`, (req, res) => {
-  // let html = '<h1>Lista firm: </h1><br>';
-  // for (const { slug, name } of companies) {
-  //   html += `<li><a href="/firmy/${slug}">${name}</a></li><br>`
-  // }
   const { name } = req.params;
   const company = companies.find(x => x.slug === name);
-  res.render('company-list', {
+  res.render('./pages/company-list', {
     title: 'Firmy',
     name: company?.name,
     companies
   });
-})
-app.get('/firmy/:name', (req, res) => {
-  debugger;
-  const { name } = req.params;
+});
 
+app.get('/firmy/:name', (req, res) => {
+  const { name } = req.params;
   console.log(chalk.bgRed(req.params.name));
   const company = companies.find(x => x.slug === name);
-  res.render('company', {
+  res.render('./pages/company', {
     name: company?.name,
     companies,
     title: company?.name ?? 'Brak Wyników'
   });
+});
 
-})
 app.get('*', (req, res) => {
   res.render('errors/404', {
-      title: 'Nic nie znaleziono!'
+      title: 'Nic nie znaleziono!',
+      layout: 'layouts/minimalistic'
   });
-})
+});
+
 app.listen(port);
 
 console.log('Program działa');
