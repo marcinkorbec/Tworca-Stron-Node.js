654c8715a95a9fc1c005061c02337537803980c6
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-11-12 13:23:58 +0100

    Update db-mongoose.js

--- Baza danych/db-mongoose.js
+++ Baza danych/db-mongoose.js
@@ -5,10 +5,22 @@ mongoose.connect('mongodb://localhost:27017/database-course');
 //model
 const Company = mongoose.model('Company', {
 	slug: {
-		type: String
+		type: String,
+		required: [true, "To pole jest wymagane"],
+		minLength: [3, 'Minimalna liczba znaków to 3'],
+		validate: (value) => {
+			if (value === "slug") {
+				throw new Error('Nazwa "slug" jest zabroniona.')
+			}
+		}
 	},
 	name: {
-		type: String
+		type: String,
+		required: [true, "Pole jest wymagane"],
+	},
+	numberEmployeesCount: {
+		type: Number,
+		min: 1
 	}
 });
 
@@ -19,8 +31,17 @@ const Company = mongoose.model('Company', {
 
 	const company = new Company({
 		name: 'TS',
-		slug: 'ts'
+		slug: 'slug'
 	})
 
-	await company.save();
+	try {
+		await company.save();
+	} catch (e) {
+		console.log('Coś poszło nie tak');
+
+		for(const key in e.errors) {
+			console.log(e.errors[key].message);
+		}
+	}
+
 })();
